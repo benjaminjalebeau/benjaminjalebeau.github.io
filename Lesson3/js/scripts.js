b2ed4c3fcd13f9b9f00565d5c9998ca959c48818
benjaminjalebeau <benjamin.ja.lebeau@gmail.com>
2021-01-23 16:31:04 -0700

    Site plan

---
+++ Lesson3/js/scripts.js
@@ -0,0 +1,6 @@
+function getyear(){
+    let d = new Date();
+    
+    document.getElementById('current_year').textContent = d.getFullYear();
+    document.getElementById('last_modified').textContent = document.lastModified;
+    }
