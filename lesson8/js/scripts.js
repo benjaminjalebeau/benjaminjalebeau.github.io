ace6cc8a452f48e85cd10535ddbc41b5ec94a2ab
benjaminjalebeau <benjamin.ja.lebeau@gmail.com>
2021-02-27 00:04:15 -0700

    asf

--- lesson8/js/scripts.js
+++ lesson8/js/scripts.js
@@ -16,7 +16,7 @@ const dayNumber = today.getDay();
 console.log(dayNumber);
 
 const element = document.getElementById("message");
-if (dayNumber == 6) {
+if (dayNumber == 5) {
     element.classList.add("showme");
 } else {
     element.classList.add("hideme");
