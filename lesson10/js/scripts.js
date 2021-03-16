154c869a4fa5a0a93891f072f7a5af26319b05f1
benjaminjalebeau <benjamin.ja.lebeau@gmail.com>
2021-03-15 22:04:06 -0600

    API

---
+++ lesson10/js/scripts.js
@@ -0,0 +1,23 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+function getyear(){
+    let d = new Date();
+    
+    document.getElementById('current_year').textContent = d.getFullYear();
+    document.getElementById('current_date').textContent = d.toDateString();
+    }
+
+const today = new Date();
+console.log(today);
+
+const dayNumber = today.getDay();
+console.log(dayNumber);
+
+const element = document.getElementById("message");
+if (dayNumber == 6) {
+    element.classList.add("showme");
+} else {
+    element.classList.add("hideme");
+}
